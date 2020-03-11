3ddd8b6bfbdad0b251c8ebac3cded42cf6455d61
LordKoen <luca.oppes1@gmail.com>
2020-03-11 13:29:53 +0000

    just testing

--- first-app/src/Note Taking App/SearchBar.js
+++ first-app/src/Note Taking App/SearchBar.js
@@ -18,4 +18,22 @@ function SearchBar(props) {
 	);
 }
 
+function SearchBar(props) {
+	return (
+		<form
+			id="searchBarForm"
+			onSubmit={(event) => event.preventDefault()}
+		>
+			<input
+				className="searchBarInput"
+				type="text"
+				placeholder="Search"
+				onChange={props.searchBarChange}
+				value={props.value}
+			/>
+		</form>
+	);
+}
+
+
 export default SearchBar;
