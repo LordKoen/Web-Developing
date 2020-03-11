e63529543568e98a3e5c37f1ba5295f53bdc693a
LordKoen <luca.oppes1@gmail.com>
2020-03-11 22:27:18 +0000

    added the helmet plugin and all the necessary link tags in index js

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -5,9 +5,30 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../styles/Main.css';
+import Helmet from 'react-helmet';
 
 function App() {
-	return <div>Hello world!</div>;
+	return (
+		<React.Fragment>
+			<Helmet>
+				<title>Josie Proto</title>
+				<link
+					rel="stylesheet"
+					href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+					integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+					crossorigin="anonymous"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css?family=Odibee+Sans&display=swap"
+					rel="stylesheet"
+				/>
+			</Helmet>
+			<header />
+			<main>
+				<div>Hello world!</div>
+			</main>
+		</React.Fragment>
+	);
 }
 
 export default App;
