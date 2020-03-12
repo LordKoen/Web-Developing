4a948a624ad3f0a9ee02cf0ad926656344434bd2
LordKoen <luca.oppes1@gmail.com>
2020-03-12 12:39:47 +0000

    everything including twitter is working

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -5,7 +5,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../styles/Main.css';
 import Helmet from 'react-helmet';
-import { window, document, exists } from 'browser-monads';
 
 function App() {
 	function fixNavbar() {
