f0b7141e28d1b84fe0ab73d724dfd253878dcfb6
LordKoen <luca.oppes1@gmail.com>
2020-03-16 20:21:37 +0000

    started programming the task that James gave me

--- practice-application/src/pages/index.js
+++ practice-application/src/pages/index.js
@@ -1,3 +1,29 @@
-import React from "react"
+import React from 'react';
+import foodList from '../../static/foods.JSON';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './index.css';
+import { Link } from 'gatsby';
 
-export default () => <div>Hello world!</div>
+function Index() {
+	let itemList = foodList.map((item) => {
+		return (
+			<Col key={Math.random()} xs="12" className="item">
+				<Link to={item.title} className="clickable">
+					<div className="center">{item.title}</div>
+					<div className="center">{item.price} </div>
+				</Link>
+			</Col>
+		);
+	});
+
+	return (
+		<Container className="mainContainer">
+			<Row className="justify-content-center">{itemList}</Row>
+		</Container>
+	);
+}
+
+export default Index;
