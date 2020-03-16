d7586fb929e7cd88d0183f5d0276f01f3bb65dd7
LordKoen <luca.oppes1@gmail.com>
2020-03-16 11:09:45 +0000

    deleted some non important files

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// import App from './App';
-// import TodoApp from "./Todo App/TodoApp"
-// import MyTodoApp from "./My Todo App/MyTodoApp"
-// import MemeGenerator from "./Meme Generator/MemeGeneratorApp"
+import App from './App';
+import TodoApp from "./Todo App/TodoApp"
+import MyTodoApp from "./My Todo App/MyTodoApp"
+import MemeGenerator from "./Meme Generator/MemeGeneratorApp"
 import NoteTakingApp from "./Note Taking App/App"
 
-ReactDOM.render(<NoteTakingApp/>, document.getElementById('root'));
+ReactDOM.render(<MyTodoApp/>, document.getElementById('root'));
