1c7def703164ce79b138b5fd572d2fac4d162211
LordKoen <luca.oppes1@gmail.com>
2020-03-23 16:31:27 +0000

    commit

--- practice-application/src/Components/orderButton.js
+++ practice-application/src/Components/orderButton.js
@@ -3,11 +3,26 @@ import Button from 'react-bootstrap/Button';
 import layoutStyles from './layout.module.css';
 
 function OrderButton(props) {
+	function handleClick() {
+		const options = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(props.cart)
+		};
+
+		fetch('http://localhost7000/', options).then((response) => {
+			console.log(response);
+		});
+	}
+
 	return (
 		<Button
 			variant="outline-primary"
 			className={layoutStyles.orderButton}
 			cart={props.cart}
+			onClick={handleClick}
 		>
 			Order
 		</Button>
